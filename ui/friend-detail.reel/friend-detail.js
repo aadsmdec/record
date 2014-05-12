55854c1977257b6e01ebd7c9610bfb410362354d
Afonso <noreply>
2014-05-12 06:23:15 +0000

    Add component friend-detail

---
+++ ui/friend-detail.reel/friend-detail.js
@@ -0,0 +1,17 @@
+/**
+ * @module ui/friend-detail.reel
+ * @requires montage/ui/component
+ */
+var Component = require("montage/ui/component").Component;
+
+/**
+ * @class FriendDetail
+ * @extends Component
+ */
+exports.FriendDetail = Component.specialize(/** @lends FriendDetail# */ {
+    constructor: {
+        value: function FriendDetail() {
+            this.super();
+        }
+    }
+});
