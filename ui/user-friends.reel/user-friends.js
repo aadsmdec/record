7456cc12fbe9bfa1907d4179614188aa3379795e
Afonso <noreply>
2014-05-12 05:54:33 +0000

    Add component user-friends

---
+++ ui/user-friends.reel/user-friends.js
@@ -0,0 +1,17 @@
+/**
+ * @module ui/user-friends.reel
+ * @requires montage/ui/component
+ */
+var Component = require("montage/ui/component").Component;
+
+/**
+ * @class UserFriends
+ * @extends Component
+ */
+exports.UserFriends = Component.specialize(/** @lends UserFriends# */ {
+    constructor: {
+        value: function UserFriends() {
+            this.super();
+        }
+    }
+});
