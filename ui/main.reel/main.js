2d60af4d368d28832f26bbd8311c54d48aa15768
Afonso <noreply>
2014-05-12 06:00:24 +0000

    Update component /ui/main.reel/

--- ui/main.reel/main.js
+++ ui/main.reel/main.js
@@ -13,5 +13,11 @@ exports.Main = Component.specialize(/** @lends Main# */ {
         value: function Main() {
             this.super();
         }
+    },
+    
+    handleAddUserButtonAction: {
+        value: function() {
+            this.templateObjects.overlay.show();
+        }
     }
 });
