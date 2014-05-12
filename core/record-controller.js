5e9c75edd874cc7173b4058f41220cbd3ae00548
Afonso <noreply>
2014-05-12 05:54:21 +0000

    Add module record-controller

---
+++ core/record-controller.js
@@ -0,0 +1,16 @@
+/**
+ * @module core/record-controller
+ * @requires montage/core/core
+ */
+var Montage = require("montage/core/core").Montage;
+/**
+ * @class RecordController
+ * @extends Montage
+ */
+exports.RecordController = Montage.specialize(/** @lends RecordController# */ {
+    constructor: {
+        value: function RecordController() {
+            this.super();
+        }
+    }
+});
